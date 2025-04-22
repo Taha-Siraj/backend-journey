ea06102fcb960c20e24e58275996d488e719eac6
tahasiraj101 <pikachugaming899@gmail.com>
2025-04-22 13:15:21 -0700

    add html

--- Server.js
+++ Server.js
@@ -2,16 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.set("view engine" , "ejs")
+app.set('view engine', "ejs")
 
 app.get("/", (req , res) => {
     res.render("index")
 } )
 app.get("/about", (req , res) => {
-    res.send("This Is about Pages")
+    res.render('about')
 } )
 app.get("/contact", (req , res) => {
-    res.send("This Is contact Pages")
+    res.render("contact")
 } )
 
 app.listen(3000)
